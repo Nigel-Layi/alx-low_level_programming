e1aa1b13eaca6238a600636e6dd01450df055ff3
Nigel-Layi <nigellayi@yahoo.com>
2023-09-10 09:04:14 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<unistd.h>
+
+/**
+ * main - prints the input
+ * Return : must return 1
+ */
+
+int main(void)
+{
+write(2, "and that piece of rt is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}

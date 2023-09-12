663a75c7ba2b90e125b2ff628ec307eee43fa529
Nigel-Layi <nigellayi@yahoo.com>
2023-09-11 18:02:52 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
- *  main- assign a random number to the variable n
+
+/**
+ * main - assign a random number to the variable n
  *  Return : 0
  */
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;

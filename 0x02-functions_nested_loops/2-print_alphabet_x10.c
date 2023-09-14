d472381909d8f501fdc43368fcbee6c4730d93f4
Nigel-Layi <nigellayi@yahoo.com>
2023-09-13 18:19:52 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet in lowercase 10 times
+ * Return: always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+			while(1 <= 9);
+		{
+			for (c = 'a'; c <= 'z'; c++)
+			{_putchar(c);
+			}
+			_putchar('\n');
+			i++;
+		}
+}
+

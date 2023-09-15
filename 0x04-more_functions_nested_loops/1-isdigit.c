eb619e5aaef81fe650f1a7aa008cb38f0a586da6
Nigel-Layi <nigellayi@yahoo.com>
2023-09-15 16:32:35 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * _isupper - check for uppercase
+ * @c: function parameter
+ * Return: 1 if uppercase
+ */
+
+int _isupper(int c)
+{
+	if (c > 'A' && c < 'Z')
+		return (1);
+	else
+		return (0);
+}

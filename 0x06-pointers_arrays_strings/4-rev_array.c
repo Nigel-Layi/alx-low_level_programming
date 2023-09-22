46c5c35ab3e4a8d1d89289716cca2a33f932b86b
Nigel-Layi <nigellayi@yahoo.com>
2023-09-22 09:58:29 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * reverse_array - main function
+ * @a: first parameter
+ * @n: second parameter
+ * Return: 0
+ */
+
+void reverse_array(int *a, int n)
+
+{
+	int k, r;
+
+	for (k = 0; k < n--; k++)
+	{
+	r = a[k];
+	a[k] = a[n];
+	a[n] = r;
+	}
+}
